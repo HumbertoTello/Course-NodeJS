76fc2a4dceb7c16ea52050cfa01d9c285f60383e
Humberto Tello <betoelo2@gmail.com>
2020-06-14 19:21:17 -0300

    Product model

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 const express = require('express'); // import of express
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 // Starting the app
 const app = express(); // express execution
 
 // Starting the DB
 // Passing the URL connection with MongoDB
-mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(
+  'mongodb://localhost:27017/nodeapi', 
+  { useNewUrlParser: true,
+    useUnifiedTopology: true });
+
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
 
 // First route
 // message on screen
 // app.get('route', (req, res))
 // '/' = root; req = requisition details to the server; res = response to the requisition
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  Product.create({
+    title: 'React Native',
+    description: 'Build native apps with React',
+    url: 'http://github.com/facebook/react-native'
+  });
+  
+  return res.send('Hello World');
 });
 
 app.listen(3001); // app will listen the 3001 port, so you can access it on your browser: "localhost:3001"
---
+++ src/models/Product.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose'); // mongoose import
+
+// description of product
+const ProductSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// registration of the model
+mongoose.model('Product', ProductSchema)
+
+
